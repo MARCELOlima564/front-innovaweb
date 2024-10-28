1309e64f8f88052dceab954830c641b61eda1147
isabela alcantara <isabela.ferreira16@aluno.senai.br>
2024-10-28 17:13:53 -0300

    feat: fazendo front funcionar e iniciando ligacao com o back

--- app/page.jsx
+++ app/page.jsx
@@ -1,38 +1,9 @@
-// src/Page.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Importa ReactDOM
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import Dashboard from './components/Dashboard';
-import { AuthProvider } from './context/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
-import './index.css'; // Estilos globais, se necessário
+// pages/index.js
 
-// Componente principal da aplicação
-const Page = () => {
+export default function Home() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>
-    </AuthProvider>
-  );
-};
-
-// Renderiza a aplicação no elemento com id 'root'
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Page />
-  </React.StrictMode>
-);
+    <div>
+      <h1>Bem-vindo ao Next.js!</h1>
+    </div>
+  )
+}
