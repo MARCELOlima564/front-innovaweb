e1d48e5988f3b063a0b9e05e202a34cd5e6a517f
Santni <nicolly.silva5@aluno.senai.br>
2024-10-30 08:06:29 -0300

    header

---
+++ app/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      {/* <a href="#" className="link">Cursos</a> */}
+      <div className="pesquisa-container">
+        <img src="/assets/lupa.png" alt="Icone de busca" className="icon" />
+        <input
+          type="text"
+          placeholder="Nome do curso"
+          className="pesquisa-input"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
