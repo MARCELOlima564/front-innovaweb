0c68b0c4e8ac03c8bf0cfbccf8ddb4daf6aed8fb
Santni <nicolly.silva5@aluno.senai.br>
2024-10-30 17:23:16 -0300

    sobrenos

--- app/Sobrenos/page.jsx
+++ app/Sobrenos/page.jsx
@@ -1,15 +1,49 @@
 import CardSobreNos from "../components/sobrenos/CardSobreNos";
+import styles from './sobrenos.module.css';
 
 export default function Sobrenos() {
     return (
-        <div>
+        <div className={styles.div1}>
+           
+            <img src="senai.webp" alt="senai" className={styles.imagem}/>
+            <h1 className={styles.titulo}>Desenvolvedores do Projeto</h1>
+        <div className={styles.Principal}>
             <CardSobreNos
                 foto={"anaju.jpg"}
                 nome={"Ana Júlia Pontes"}
                 funcao={"Desenvolvedora"}
                 desc={"Tem 18 anos, estudante formada do SENAI Valinhos, no curso de Desenvolvimento de Sistemas em 2024."}
-                email={"anamondinip@gmail.com"}
+                email={"ana-pontes@aluno.senai.br"}
             />
+             <CardSobreNos
+                foto={"isabela.jpg"}
+                nome={"Isabela Alcântara"}
+                funcao={"Desenvolvedora"}
+                desc={"Tem 18 anos, estudante formada do SENAI Valinhos, no curso de Desenvolvimento de Sistemas em 2024."}
+                email={"isabela.ferreira16@aluno.senai.br"}
+            />
+              <CardSobreNos
+                foto={"marcelo.jpg"}
+                nome={"Marcelo Gabriel Lima"}
+                funcao={"Desenvolvedor"}
+                desc={"Tem 18 anos, estudante formado do SENAI Valinhos, no curso de Desenvolvimento de Sistemas em 2024."}
+                email={"marcelo.lima4@aluno.senai.br"}
+            />
+             <CardSobreNos
+                foto={"nicolly.jpg"}
+                nome={"Nicolly Santos"}
+                funcao={"Desenvolvedora"}
+                desc={"Tem 17 anos, estudante formada do SENAI Valinhos, no curso de Desenvolvimento de Sistemas em 2024."}
+                email={"nicolly.silva5@aluno.senai.br"}
+            />
+            <CardSobreNos
+                foto={"manuela.jpg"}
+                nome={"Manuela Queiroz"}
+                funcao={"Desenvolvedora"}
+                desc={"Tem 17 anos, estudante formada do SENAI Valinhos, no curso de Desenvolvimento de Sistemas em 2024."}
+                email={"manuelamachado@aluno.senai.br"}
+            />
+        </div>
         </div>
     )
 }
