c57e764b5cb5a098ea754721a47b6010320bd188
Marcelo Gabriel <marcelo.lima4@aluno.senai.br>
2024-11-06 09:50:07 -0300

    Paina Faq e container contato

--- app/Faq/page.jsx
+++ app/Faq/page.jsx
@@ -11,13 +11,13 @@ export default function Faq() {
             <div className={styles.containerPerguntas}>
 
             <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
-            <CardFaq perguntas={"O que o senai?"}
+            <CardFaq perguntas={"O QUE É O SENAI?"}
                      respostas={"Uma escola de curso tecnico focado com o profissonal."}/>
             </div>  
             <div className={styles.containerPerguntas}>
 
             <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
-            <CardFaq perguntas={"O que o senai?"}
+            <CardFaq perguntas={"O QUE É O SENAI?"}
                      respostas={"Uma escola de curso tecnico focado com o profissonal."}/>
             
 
@@ -25,13 +25,22 @@ export default function Faq() {
             <div className={styles.containerPerguntas}>
 
             <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
-            <CardFaq perguntas={"O que o senai?"}
+            <CardFaq perguntas={"O QUE É O SENAI?"}
                      respostas={"Uma escola de curso tecnico focado com o profissonal."}/>
             </div>   
 
             <div className={styles.containerContato}>
                 <h1 className={styles.tituloContato}>Contato</h1>
                 <p className={styles.subtiuloContato}>Informações de contato</p>
+                <div className={styles.containerInformacoes}>
+                    <div className={styles.divInformacoes}>
+                    <p className={styles.telefone}>19 989256337</p>
+                    <p className={styles.localizacao}>R. Artur Fernandes Querido, 55 - Vila Santo Antonio, Valinhos - SP, 13270-530</p>
+                    </div>
+                    <div className={styles.divImagem}>
+                    <img src="senai2.jpg" alt="senai2" className={styles.imagem} />
+                    </div>
+                </div>
             </div>            
         </div>
     )
