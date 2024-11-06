27fce8ce9de0788af2bc75841fe6b455b9c55d8e
Marcelo Gabriel <marcelo.lima4@aluno.senai.br>
2024-11-06 17:15:26 -0300

    OK

--- app/Faq/page.jsx
+++ app/Faq/page.jsx
@@ -3,45 +3,67 @@ import { RiCheckboxBlankCircleFill } from "react-icons/ri";
 import CardFaq from "../components/cardFaq/CardFaq";
 
 export default function Faq() {
-    return(
+    return (
         <div className={styles.container}>
+<div className={styles.containerT}>
+            <h1 className={styles.titulo}>Perguntas frequentes</h1>
+            </div>
+            <div className={styles.perguntas}>
+                <div className={styles.containerPerguntas}>
 
-         <h1 className={styles.titulo}>Perguntas frequentes</h1>
+                    <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
+                    <CardFaq perguntas={"O QUE É O SENAI?"}
+                        respostas={"Uma escola de curso tecnico focado com o profissonal."} />
+                </div>
+                <div className={styles.containerPerguntas}>
 
-            <div className={styles.containerPerguntas}>
+                    <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
+                    <CardFaq perguntas={"O QUE É O SENAI?"}
+                        respostas={"Uma escola de curso tecnico focado com o profissonal."} />
 
-            <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
-            <CardFaq perguntas={"O QUE É O SENAI?"}
-                     respostas={"Uma escola de curso tecnico focado com o profissonal."}/>
-            </div>  
-            <div className={styles.containerPerguntas}>
 
-            <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
-            <CardFaq perguntas={"O QUE É O SENAI?"}
-                     respostas={"Uma escola de curso tecnico focado com o profissonal."}/>
-            
+                </div>
+                <div className={styles.containerPerguntas}>
 
-            </div>     
-            <div className={styles.containerPerguntas}>
+                    <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
+                    <CardFaq perguntas={"O QUE É O SENAI?"}
+                        respostas={"Uma escola de curso tecnico focado com o profissonal."} />
+                </div>
+            </div>
+            <div className={styles.perguntas}>
+                <div className={styles.containerPerguntas}>
 
-            <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
-            <CardFaq perguntas={"O QUE É O SENAI?"}
-                     respostas={"Uma escola de curso tecnico focado com o profissonal."}/>
-            </div>   
+                    <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
+                    <CardFaq perguntas={"O QUE É O SENAI?"}
+                        respostas={"Uma escola de curso tecnico focado com o profissonal."} />
+                </div>
+                <div className={styles.containerPerguntas}>
 
+                    <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
+                    <CardFaq perguntas={"O QUE É O SENAI?"}
+                        respostas={"Uma escola de curso tecnico focado com o profissonal."} />
+
+
+                </div>
+                <div className={styles.containerPerguntas}>
+
+                    <RiCheckboxBlankCircleFill color="red" className={styles.circulo} />
+                    <CardFaq perguntas={"O QUE É O SENAI?"}
+                        respostas={"Uma escola de curso tecnico focado com o profissonal."} />
+                </div>
+            </div>
             <div className={styles.containerContato}>
                 <h1 className={styles.tituloContato}>Contato</h1>
                 <p className={styles.subtiuloContato}>Informações de contato</p>
                 <div className={styles.containerInformacoes}>
                     <div className={styles.divInformacoes}>
-                    <p className={styles.telefone}>19 989256337</p>
-                    <p className={styles.localizacao}>R. Artur Fernandes Querido, 55 - Vila Santo Antonio, Valinhos - SP, 13270-530</p>
+                        <p className={styles.telefone}>TELEFONE: (19) 989256337</p>
                     </div>
-                    <div className={styles.divImagem}>
-                    <img src="senai2.jpg" alt="senai2" className={styles.imagem} />
-                    </div>
-                </div>
-            </div>            
+                    <div className={styles.divInformacoes}></div>
+                    <p className={styles.localizacao}>LOCALIZAÇÃO: R. Artur Fernandes Querido, 55 - Vila Santo Antonio, Valinhos - SP, 13270-530 </p>
+                   </div>
+
+            </div>
         </div>
     )
 }
