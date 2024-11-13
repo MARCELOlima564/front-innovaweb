026e7c5816122aa8815a99ae826d8c3e190a1c23
aninha306 <124887301+aninha306@users.noreply.github.com>
2024-11-13 11:13:28 -0300

    sobrenos fim

--- app/components/CarrosselSobreNos/CarouselSobre.jsx
+++ app/components/CarrosselSobreNos/CarouselSobre.jsx
@@ -6,41 +6,61 @@ const CarouselComponent = () => (
     <div className={styles.carouselWrapper}>
         <Carousel arrows infinite={true} className={styles.container}>
             <div className={styles.carouselItem}>
+                <div className={styles.card}>
                 <img src="anaju.jpg" alt="Imagem 1" className={styles.carouselImage} />
+                <div className={styles.infos}>
                 <h1 className={styles.Nome}>Ana Júlia Pontes</h1>
                 <h3 className={styles.funcoes}>Desenvolvedora</h3>
+                </div>
+                </div>
                 <p className={styles.descricao}>Tem 18 anos, estudante formada do SENAI Valinhos,
                     no curso de Desenvolvimento de Sistemas em 2024.</p>
                 <p className={styles.email}>ana-pontes@aluno.senai.br</p>
             </div>
             <div className={styles.carouselItem}>
+            <div className={styles.card}>
                 <img src="isabela.jpg" alt="Imagem 2" className={styles.carouselImage} />
+                <div className={styles.infos}>
                 <h1 className={styles.Nome}>Isabela Alcântara</h1>
                 <h3 className={styles.funcoes}>Desenvolvedora</h3>
+                </div>
+                </div>
                 <p className={styles.descricao}>Tem 18 anos, estudante formada do SENAI Valinhos,
                     no curso de Desenvolvimento de Sistemas em 2024.</p>
                 <p className={styles.email}>isabela.ferreira16@aluno.senai.br</p>
             </div>
             <div className={styles.carouselItem}>
+            <div className={styles.card}>
                 <img src="marcelo.jpg" alt="Imagem 3" className={styles.carouselImage} />
+                <div className={styles.infos}>
                 <h1 className={styles.Nome}>Marcelo Lima</h1>
                 <h3 className={styles.funcoes}>Desenvolvedor</h3>
+                </div>
+                </div>
                 <p className={styles.descricao}>Tem 18 anos, estudante formado do SENAI Valinhos,
                     no curso de Desenvolvimento de Sistemas em 2024.</p>
                 <p className={styles.email}>marcelo.lima4@aluno.senai.br</p>
             </div>
             <div className={styles.carouselItem}>
+            <div className={styles.card}>
                 <img src="nicolly.jpg" alt="Imagem 4" className={styles.carouselImage} />
+                <div className={styles.infos}>
                 <h1 className={styles.Nome}>Nicolly Santos</h1>
                 <h3 className={styles.funcoes}>Desenvolvedora</h3>
+                </div>
+                </div>
                 <p className={styles.descricao}>Tem 17 anos, estudante formada do SENAI Valinhos,
                     no curso de Desenvolvimento de Sistemas em 2024.</p>
                 <p className={styles.email}>nicolly.silva5@aluno.senai.br</p>
             </div>
             <div className={styles.carouselItem}>
+            <div className={styles.card}>
                 <img src="manuela.jpg" alt="Imagem 5" className={styles.carouselImage} />
+                <div className={styles.infos}>
                 <h1 className={styles.Nome}>Manuela Queiroz</h1>
                 <h3 className={styles.funcoes}>Desenvolvedora</h3>
+                </div>
+                </div>
                 <p className={styles.descricao}>Tem 17 anos, estudante formada do SENAI Valinhos,
                     no curso de Desenvolvimento de Sistemas em 2024.</p>
                 <p className={styles.email}>manuelamachado@aluno.senai.br</p>
