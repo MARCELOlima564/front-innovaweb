ab677e820b97f8a452d646660c3e0617f8265ae9
Manuela Machado <manuelamachado@aluno.senai.br>
2024-11-13 16:41:48 -0300

    .

--- app/components/cardFaq/CardFaq.jsx
+++ app/components/cardFaq/CardFaq.jsx
@@ -1,17 +1,57 @@
-import styles from "./cardfaq.module.css"
+import React from 'react';
+import { Carousel } from 'antd';
+import styles from './CarouselSobre.module.css';
 
-const CardFaq = ({ perguntas, respostas }) => {
-    return (
-        <div>
-        <div className={styles.containerPerguntas}>
+const CarouselComponent = () => (
+    <div className={styles.carouselWrapper}>
+        <Carousel arrows infinite={true} className={styles.container}>
+            <div className={styles.carouselItem}>
+                <div className={styles.infos}>
+                <h1 className={styles.pergunta}>Ana Júlia Pontes</h1>
+                </div>
+                <p className={styles.resposta}>Tem 18 anos, estudante formada do SENAI Valinhos,
+                    no curso de Desenvolvimento de Sistemas em 2024.</p>
+            </div>
+            <div className={styles.carouselItem}>
+                <div className={styles.infos}>
+                <h1 className={styles.pergunta}>Isabela Alcântara</h1>
+                </div>
+                <p className={styles.resposta}>Tem 18 anos, estudante formada do SENAI Valinhos,
+                    no curso de Desenvolvimento de Sistemas em 2024.</p>
+            </div>
+            <div className={styles.carouselItem}>
+                <div className={styles.infos}>
+                <h1 className={styles.pergunta}>Marcelo Lima</h1>
+                </div>
+                <p className={styles.resposta}>Tem 18 anos, estudante formado do SENAI Valinhos,
+                    no curso de Desenvolvimento de Sistemas em 2024.</p>
+            </div>
+            <div className={styles.carouselItem}>
+                <div className={styles.infos}>
+                <h1 className={styles.pergunta}>Nicolly Santos</h1>
+                </div>
+                <p className={styles.resposta}>Tem 17 anos, estudante formada do SENAI Valinhos,
+                    no curso de Desenvolvimento de Sistemas em 2024.</p>
+                <p className={styles.email}>nicolly.silva5@aluno.senai.br</p>
+            </div>
+            <div className={styles.carouselItem}>
+                <div className={styles.infos}>
+                <h1 className={styles.pergunta}>Manuela Queiroz</h1>
+                </div>
+                <p className={styles.resposta}>Tem 17 anos, estudante formada do SENAI Valinhos,
+                    no curso de Desenvolvimento de Sistemas em 2024.</p>
+                <p className={styles.email}>manuelamachado@aluno.senai.br</p>
+            </div>
+            <div className={styles.carouselItem}>
+                <div className={styles.infos}>
+                <h1 className={styles.pergunta}>Manuela Queiroz</h1>
+                </div>
+                <p className={styles.resposta}>Tem 17 anos, estudante formada do SENAI Valinhos,
+                    no curso de Desenvolvimento de Sistemas em 2024.</p>
+                <p className={styles.email}>manuelamachado@aluno.senai.br</p>
+            </div>
+        </Carousel>
+    </div>
+);
 
-            <p className={styles.Perguntas}>{perguntas}</p>
-            </div>
-            <div>
-                <p className={styles.Respostas}>{respostas}</p>
-            </div>
-            </div>
-    )
-  }
-  
-  export default CardFaq;
+export default CarouselComponent;
